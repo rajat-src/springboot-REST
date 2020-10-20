2c3a8248933286bf0b6d8a0aca3637450167e04d
rajat-src <rajatchitgupi@gmail.com>
2020-10-21 00:51:39 +0530

    simple working REST with postgresql

---
+++ springrestrial/src/test/java/com/springpostgres/springrestrial/SpringrestrialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springpostgres.springrestrial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringrestrialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
